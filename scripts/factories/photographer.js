ec4fa58981cf8c8896e91a97339ff0e46b694419
Pascal Allau <pascal.allau@etu.toulouse-inp.fr>
2022-05-19 19:14:47 +0200

    refactoring functions in photographer module

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,4 +1,28 @@
 /**
+ * It creates an element, sets its text content, sets its attributes, sets its class name, and
+ * appends it to a parent element
+ * @param   {String}  tagName - the name of the tag you want to create.
+ * @param   {String}  text - the text content of the element
+ * @param   {Object}  attributValue - an object containing the attributes and their values
+ * @param   {String}  className - the class name of the element
+ * @param   {HTMLElement} tagParent - the parent element of the element to be created
+ */
+function createElement(
+  tagParent,
+  tagName,
+  text = '',
+  attributValue = {},
+  className = ''
+) {
+  const elt = document.createElement(tagName);
+  elt.textContent = text;
+  Object.entries(attributValue).forEach(([key, value]) => {
+    elt.setAttribute(key, value);
+  });
+  elt.className = className;
+  tagParent.appendChild(elt);
+}
+/**
  * It takes an object as an argument and returns an object with two properties:
  *
  * data: the object passed as an argument
@@ -10,7 +34,7 @@
  * @return   {Object} An object with two properties: data and getUserCardDOM.
  */
 function photographerFactory(data) {
-  const { name, portrait, city, country, tagline, price } = data;
+  const { id, name, portrait, city, country, tagline, price } = data;
   const picture = `assets/photographers/${portrait}`;
   /**
    * It creates a DOM element for a user card.
@@ -18,35 +42,11 @@ function photographerFactory(data) {
    */
   function getUserCardDOM() {
     const article = document.createElement('article');
-    /**
-     * It creates an element, sets its text content, sets its attributes, sets its class name, and
-     * appends it to a parent element
-     * @param   {String}  tagName - the name of the tag you want to create.
-     * @param   {String}  text - the text content of the element
-     * @param   {Object}  attributValue - an object containing the attributes and their values
-     * @param   {String}  className - the class name of the element
-     * @param   {HTMLElement} tagParent - the parent element of the element to be created
-     */
-    function createElement(
-      tagName,
-      text = '',
-      attributValue = {},
-      className = '',
-      tagParent = article
-    ) {
-      const elt = document.createElement(tagName);
-      elt.textContent = text;
-      Object.entries(attributValue).forEach(([key, value]) => {
-        elt.setAttribute(key, value);
-      });
-      elt.className = className;
-      tagParent.appendChild(elt);
-    }
-    createElement('img', '', { src: picture });
-    createElement('h2', name);
-    createElement('div', `${city}, ${country}`);
-    createElement('p', tagline);
-    createElement('span', `${price}€/jour`);
+    createElement(article, 'img', '', { src: picture, id });
+    createElement(article, 'h2', name);
+    createElement(article, 'div', `${city}, ${country}`);
+    createElement(article, 'p', tagline);
+    createElement(article, 'span', `${price}€/jour`);
     return article;
   }
   return {
@@ -54,4 +54,4 @@ function photographerFactory(data) {
     getUserCardDOM
   };
 }
-export default photographerFactory;
+export { photographerFactory, createElement };
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,10 +1,10 @@
 import '../../css/style.scss';
-import photographerFactory from '../factories/photographer';
+import { photographerFactory } from '../factories/photographer';
 
 /**
  * It fetches the data from the photographers.json file, parses the data into JSON, and returns the
  * photographers array
- * @returns  {Promise.<{ name: string, id: number, city: string, country: string, tagline: string, price: number, portrait: string}>}  An array of objects.
+ * @returns  {Promise.<{ name: string, id: number, city: string, country: string, tagline: string, price: number, portrait: string}[]>}  An array of objects.
  */
 async function getPhotographers() {
   const res = await fetch('data/photographers.json');
